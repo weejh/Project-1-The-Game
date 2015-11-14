d6ab9e4e2482e9af09acfbd7dada188e661aa4cd
weejh <wee_jh@yahoo.com>
2015-11-14 10:28:30 +0800

    updated

--- develop/four.js
+++ develop/four.js
@@ -1,13 +1,17 @@
 var currentPlayer = 'one'
-var board = document.querySelector('tileboard')
-
+var body = document.querySelector('body')
+console.log(board);
 board.addEventListener('click', event => {
-  var tileboard = event.target
+  var board = event.target
   console.log(event)
-  if (tileboard.textContent) return
+//  if (board.textContent) return
   if (currentPlayer === 'one') {
+    console.log(currentPlayer);
     currentPlayer = 'two'
+
   } else {
+    console.log(currentPlayer);
     currentPlayer = 'one'
+
   }
 })
