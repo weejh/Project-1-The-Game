ac1419348fe049e930f1515d7e6fa8fa20be4f74
weejh <wee_jh@yahoo.com>
2015-11-14 20:35:55 +0800

    highlight column when mouse over

--- develop/four.js
+++ develop/four.js
@@ -1,8 +1,11 @@
 // var currentPlayer = 'one'
 var body = document.querySelector('body')
+var pastColumn = ''
+var currentColumn = ''
 // console.log(body)
 body.addEventListener('click', clickevent)
 body.addEventListener('mouseover', mouseoverevent)
+body.addEventListener('mouseout', mouseoutevent)
 
 function clickevent () {
   console.log('clickevnet')
@@ -13,10 +16,37 @@ function mouseoverevent () {
 //  console.log('mouseover')
   var board = event.target
   if (board.className !== 'tileboard') return
-  console.log(board.className)
-
+//  console.log(board.className)
+//  console.log(board.id.charAt(board.id.length - 1))
+//  highlightColum (board.id.charAt(board.id.length - 1))
+//currentColumn = board.id
+//console.log('current : ' + board.id);
+highlightColumn (board.id, 'white')
 }
 
+function mouseoutevent () {
+  var board = event.target
+  if (board.className !== 'tileboard') return
+//  console.log('past : ' + board.id );
+  highlightColumn (board.id, 'black')
+}
+function highlightColumn (Col, Color) {
+   var colarray = ['chip', '0', '1', '2', '3', '4', '5', '6']
+  // console.log(pastColumnelement);
+  // pastColumnelement.style.backgroundColor='black'
+colarray.forEach( element  => {
+  //console.log(element + Col.charAt(Col.length-1))
+  Columnelement=document.getElementById(element + Col.charAt(Col.length-1))
+  // console.log(Columnelement)
+  Columnelement.style.backgroundColor=Color
+
+})
+
+//   pastColumnelement = currentColumnelement || ''
+//   colarray.forEach ((Col) => {
+//     board.id
+//   })
+}
 /* event => {
   var tileboard = event.target
   console.log(event)
