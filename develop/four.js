d98111c51a9012359b7a958b765bfc277088fc07
weejh <wee_jh@yahoo.com>
2015-11-18 09:37:22 +0800

    added - check for winner

--- develop/four.js
+++ develop/four.js
@@ -1,3 +1,4 @@
+//import { checkWinner (givenLoc, statusBoard, currentPlayer) }from 'module.js'
 var currentPlayer = 'one'
 var body = document.querySelector('body')
 var statusBoard = {}
@@ -52,7 +53,6 @@ function DropChip (Col) {
   if (statusArray.lastIndexOf('one') < statusArray.lastIndexOf('two')) updateStat(currentPlayer, statusArray.lastIndexOf('two'), Col)
 //
 // next player
-  document.getElementById('left4status').textContent = 'Player ' + currentPlayer.charAt(0).toUpperCase() + currentPlayer.slice(1)
 //  console.log(statusBoard)
 //console.log('current player : ' + currentPlayer + ' | location :' + (statusArray.lastIndexOf(currentPlayer) + 1).toString(10) + Col.charAt(Col.length - 1))
   // to check for winner
@@ -61,6 +61,9 @@ function DropChip (Col) {
 checkWinner ((statusArray.lastIndexOf(currentPlayer) + 1).toString(10) + Col.charAt(Col.length - 1), currentPlayer)
 
 currentPlayer === 'one' ? currentPlayer = 'two' : currentPlayer = 'one'
+document.getElementById('left4status').textContent = 'Player ' + currentPlayer.charAt(0).toUpperCase() + currentPlayer.slice(1)
+//document.getElementById('left4status').style.co
+
 }
 
 function updateStat (player, row, column) {
@@ -71,30 +74,25 @@ function updateStat (player, row, column) {
 
 function checkWinner (Col, player) {
   // always check for next 3 location in all direction
-  var colgiven = parseInt(Col.charAt(Col.length - 1), 10)
-  var rowgiven = parseInt(Col.charAt(Col.length - Col.length), 10)
+  var rowgiven = parseInt(Col.charAt(Col.length - 1), 10)
+  var colgiven = parseInt(Col.charAt(Col.length - Col.length), 10)
   var indexarray = [-1, 0, 1]
   var arrayprocess = Array.of(3)
+  var arraycoordinate = Array.of(3)
   var Astatus = []
-  //console.log('in check winner, location: ' + colgiven + rowgiven + ' | player : ' + player);
-  console.log(statusBoard);
+  var winner = '0'
+  console.log('in check winner, location: ' + colgiven + rowgiven + ' | player : ' + player)
+  console.log(statusBoard)
   indexarray.forEach(rowIndex => {
     indexarray.forEach(columnIndex => {
       [1, 2, 3].forEach((element, elementIndex) => {
-          if ((columnIndex.toString(10) + rowIndex.toString(10)) !== '00') {
-    //  arrayprocess[elementIndex] = (columnIndex*element).toString(10) + (rowIndex*element).toString(10)
-
-     arrayprocess[elementIndex] = statusBoard[((rowIndex * element) + colgiven).toString(10) + ((columnIndex * element) + rowgiven).toString(10)]
-  //  console.log(typeof(arrayprocess[elementIndex]));
-  console.log('col : ' + (columnIndex * element) + ' | row : ' + (rowIndex * element) + ' | arrayprocess ' + arrayprocess)
-  }
-
+        arraycoordinate[elementIndex] = (((columnIndex * element) + colgiven).toString(10) + ((rowIndex * element) + rowgiven).toString(10))
+        arrayprocess[elementIndex] = statusBoard[((columnIndex * element) + colgiven).toString(10) + ((rowIndex * element) + rowgiven).toString(10)]
       })
-      console.log(Astatus);
-      if (arrayprocess.every(ele => ele === player)) Astatus.push('matched')
-    //  console.log('status : ' + status);
-      arrayprocess = Array.of(3)
+     console.log('player:' + player + ' | loc : '+ arraycoordinate + ' | arrayprocess : ' + arrayprocess )
+      Astatus.push(arraycoordinate.every(cord => cord !== Col) && arrayprocess.every(ele => ele === player))
+      arraycoordinate = Array.of(3)
     })
   })
-  return Astatus
+  console.log(Astatus)
 }
